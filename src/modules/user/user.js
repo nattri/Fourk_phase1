c416848c66faa2ebc71713e22df06619560e3f87
nattri <nvn.atri@gmail.com>
2016-03-21 23:30:33 +0530

    NAttri: CRUD operation implementation basics

--- src/modules/user/user.js
+++ src/modules/user/user.js
@@ -10,6 +10,10 @@ router.route('/')
     .get(getUsers)
     .post(saveUser);
 
+router.route('/:id')
+    .delete(deleteUser)
+    .put(updateUser);
+
 function getUsers(req, res) {
     userService.getUsers(function (err, data) {
         if (err) {
@@ -23,10 +27,8 @@ function getUsers(req, res) {
 }
 
 function saveUser(req, res) {
-    /* TODO: this will be dynamic data from req */
     var user = {
-        name: 'Fourk',
-        id: 1
+        name: req.body.name
     };
     userService.saveUser(user, function (err) {
         if (err) {
@@ -34,9 +36,36 @@ function saveUser(req, res) {
             res.json({type: 'error', msg: err});
         } else {
             console.log('user saved successfully');
-            res.json({type:'success'});
+            res.json({type: 'success'});
         }
     });
+}
+
+function deleteUser(req, res) {
+    var id = req.params.id;
+    userService.deleteUser(id, function (err) {
+        if (err) {
+            console.dir('Error in deleting user');
+            res.json({type: 'error', msg: err});
+        }
+        console.dir('User removed.');
+        res.json({type: 'success'});
+    });
+}
+
+function updateUser(req, res) {
+    var id = '56c99353c3c657441ddffd51';
+    var obj = {
+        name: req.body.name
+    };
+    userService.updateUser(id, obj, function (err) {
+        if(err) {
+            console.dir('Error in updating user');
+            res.json({type: 'error', msg: err});
+        }
+        console.dir('User updated successfully..!');
+        res.json({type: 'success'});
+    })
 }
 
 module.exports = router;
--- src/modules/user/user.service.js
+++ src/modules/user/user.service.js
@@ -4,22 +4,38 @@ var User = require('./user.model');
 
 module.exports = {
     getUsers: getUsers,
-    saveUser: saveUser
+    saveUser: saveUser,
+    deleteUser: deleteUser,
+    updateUser: updateUser
 };
 
 function getUsers(callback) {
     User.find(function (err, data) {
-        if(callback){
+        if (callback) {
             callback(err, data);
         }
     });
 }
 
-function saveUser(obj, callback){
+function saveUser(obj, callback) {
     var user = new User(obj);
     user.save(function (err, data) {
-        if(callback){
+        if (callback) {
             callback(err, data);
+        }
+    });
+}
+
+function deleteUser(id, callback) {
+    User.findById(id, function (err, doc) {
+        doc.remove(callback);
+    });
+}
+
+function updateUser(id, obj, callback) {
+    User.findByIdAndUpdate(id, obj, function (err, doc) {
+        if (callback) {
+            callback(err, doc);
         }
     });
 }
