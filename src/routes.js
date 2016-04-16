84cb426d4164e8097bebb5402e7f11217a536790
sweta <shweta16690@gmail.com>
2016-04-16 16:51:17 +0530

    my first change

--- src/routes.js
+++ src/routes.js
@@ -11,4 +11,6 @@ module.exports = function (app) {
     //app.use('/api/login', require('./modules/login/login'));
     app.use('/api/user', require('./modules/user/user'));
 
+
+    /* code changed shw*/
 };
